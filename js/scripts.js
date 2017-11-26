e92a718afe47428a0b00bc765728dad5c0c12d8e
Bartosz Kuziara <bartosz.kuziara@gmail.com>
2017-11-26 12:19:14 +0100

    Added scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = prompt('A = ');
+	h = prompt('H = ');
+	tringleArea = a*h/2;
+
+
+alert('Triangle field with base a: ' + a + ' and height h : ' + h + ' is to: ' + tringleArea);
+console.log('Triangle field with base a: ' + a + ' and height h : ' + h + ' is to: ' + tringleArea);
